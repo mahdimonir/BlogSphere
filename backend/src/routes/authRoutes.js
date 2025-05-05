5740bd8cafb4993adc088fc1e433152c46faf9f2
Moniruzzaman Mahdi <159605851+mahdimonir@users.noreply.github.com>
2025-05-05 20:14:58 +0600

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,21 +8,30 @@ import {
   resendOtp,
   resetPassword,
   signup,
+  updateAvatar,
+  updateUser,
   verifyAccount,
 } from "../controllers/authController.js";
-import { authorized, verifyJWT } from "../middleware/authMiddleware.js";
+import { verifyJWT } from "../middleware/authMiddleware.js";
+import { upload } from "../middleware/multerMiddleware.js";
 
 const router = Router();
 
+// Public routes
 router.route("/signup").post(signup);
 router.route("/verify").post(verifyAccount);
 router.route("/resend-otp").post(resendOtp);
 router.route("/login").post(login);
-router.route("/logout").post(verifyJWT, logout);
-router.route("/refresh-token").post(refreshAccessToken);
 router.route("/forget-password").post(forgetPassword);
 router.route("/reset-password").post(resetPassword);
-router.route("/update").put(authorized, updateUser);
-router.route("/delece").delete(authorized, deleteUser);
+
+// Protected routes
+router.route("/logout").post(verifyJWT, logout);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/update-user").patch(verifyJWT, updateUser);
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route("/delete-user").delete(verifyJWT, deleteUser);
 
 export default router;
